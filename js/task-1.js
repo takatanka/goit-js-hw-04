9ce224a7e6df124274dfc1c3f422bb265bca03c2
takatanka <tashka.falcon@gmail.com>
2024-01-04 17:12:16 +0100

    v1

---
+++ js/task-1.js
@@ -0,0 +1,33 @@
+function isEnoughCapacity(products, containerSize) {
+    
+    let items = Object.values(products);
+    let totalItems = 0;
+    
+    for (let item of items) {
+        totalItems += item;
+    };
+    let result;
+    
+    if (totalItems <= containerSize) {
+        result = true;
+    } else {
+        result = false; 
+    }
+    return result;
+}
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
